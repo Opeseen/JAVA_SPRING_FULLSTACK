0a2dfe94d08ce78d5e1f9ded4c4baadd6be29c50
Opeyemi <Opeyemi5055@yahoo.com>
2025-07-10 15:15:01 +0100

    section7

--- section7/eazystore/src/main/java/com/eazybytes/eazystore/controller/ProductController.java
+++ section7/eazystore/src/main/java/com/eazybytes/eazystore/controller/ProductController.java
@@ -1,14 +1,25 @@
 package com.eazybytes.eazystore.controller;
 
+import com.eazybytes.eazystore.dto.ProductDTO;
+import com.eazybytes.eazystore.entity.Product;
+import com.eazybytes.eazystore.repository.ProductRepository;
+import com.eazybytes.eazystore.service.IProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("api/v1/products")
 public class ProductController {
+  private final IProductService iProductService;
+
   @GetMapping
-  public String getProducts(){
-    return "Here are your products";
+  public List<ProductDTO> getProducts(){
+    List<ProductDTO> productList = iProductService.getProducts();
+    return productList;
   }
 }

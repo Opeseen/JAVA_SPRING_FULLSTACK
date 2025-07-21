69d1598454b46d2bc50310054c6b7bba806c1b7c
Opeyemi <Opeyemi5055@yahoo.com>
2025-07-21 16:01:26 +0100

    sec7

--- section7/eazystore/src/main/java/com/eazybytes/eazystore/controller/ProductController.java
+++ section7/eazystore/src/main/java/com/eazybytes/eazystore/controller/ProductController.java
@@ -1,11 +1,8 @@
 package com.eazybytes.eazystore.controller;
 
 import com.eazybytes.eazystore.dto.ProductDTO;
-import com.eazybytes.eazystore.entity.Product;
-import com.eazybytes.eazystore.repository.ProductRepository;
 import com.eazybytes.eazystore.service.IProductService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- section8/eazystore-ui/src/components/ProductListings.jsx
+++ section8/eazystore-ui/src/components/ProductListings.jsx
@@ -1,9 +1,34 @@
 import React from "react";
 import ProductCard from "./ProductCard";
+import SearchBox from "./SearchBox";
+import DropDown from "./DropDown";
+
+const sortList = ["Popularity", "Price Low to High", "Price High to Low"];
 
 export default function ProductListings({ products }) {
+  let searchText = "";
+  // define the search function
+  function handleSearchChange(inputSearch, event) {
+    searchText = inputSearch;
+    console.log(searchText);
+    console.log(event);
+  }
+
   return (
     <div className="max-w-[1152px] mx-auto">
+      <div className="flex flex-col sm:flex-row justify-between items-canter gap-4 pt-12">
+        <SearchBox
+          label={"Search"}
+          placeholder={"Search products..."}
+          value={searchText}
+          handleSearch={(value, event) => handleSearchChange(value, event)}
+        />
+        <DropDown
+          label={"sort by"}
+          options={sortList}
+          selectedValue={"Popularity"}
+        />
+      </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-8 gap-x-6 py-12">
         {/* using ternary operator */}
         {products.length > 0 ? (

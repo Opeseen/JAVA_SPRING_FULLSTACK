01439a766a152cb003e4d13b4cca58374ca709d5
Opeyemi <Opeyemi5055@yahoo.com>
2025-08-01 20:18:20 +0100

    mdified sec 10

---
+++ section10/v2_eazystore-ui/src/main.jsx
@@ -0,0 +1,35 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
+import About from "./components/About.jsx";
+import Contact from "./components/Contact.jsx";
+import Cart from "./components/Cart.jsx";
+import Login from "./components/Login.jsx";
+import Home, { productsLoader } from "./components/Home.jsx";
+import ErrorPage from "./components/ErrorPage.jsx";
+
+const routeDefinitions = createRoutesFromElements(
+  <Route path="/" element={<App />} errorElement={<ErrorPage />}>
+    <Route index element={<Home />} loader={productsLoader} />
+    <Route path="/home" element={<Home />} loader={productsLoader} />
+    <Route path="/about" element={<About />} />
+    <Route path="/contact" element={<Contact />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/cart" element={<Cart />} />
+  </Route>
+);
+
+const appRouter = createBrowserRouter(routeDefinitions);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={appRouter} />
+  </StrictMode>
+);
